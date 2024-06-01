55001e14484123252dd3089ff6d1f94a45da58da
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2024-06-01 21:20:02 +0200

    1/6/24 after install of of alpha for strating screen

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,23 @@
+return {
+    "goolord/alpha-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.startify")
+
+        local logo = [[
+         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+        ]]
+
+        dashboard.section.header.val = vim.split(logo, "\n")
+
+        alpha.setup(dashboard.opts)
+    end,
+}
