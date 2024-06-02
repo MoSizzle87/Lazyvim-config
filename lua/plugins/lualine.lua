375e4dd9f6bad57c6c8f1ca5cb7f4415c76595c4
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2024-06-02 20:03:02 +0200

    Add of tabline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,21 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    config = function()
-        require('lualine').setup({
-            options = {
-                theme = 'tokyonight'
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "tokyonight",
+			},
+
+			-- Lualine tabline support
+			tabline = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { require("tabline").tabline_buffers },
+				lualine_x = { require("tabline").tabline_tabs },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			-- sections = { lualine_c = { require("auto-session.lib").current_session_name } },
+		})
+	end,
 }
-
-
-
