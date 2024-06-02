464a5d2fd296baf3550a8c5498f8fe8cc799d47b
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2024-06-02 20:14:27 +0200

    2/6/24 Add of tabline

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("tabline").setup({
 			-- Defaults configuration options
-			enable = false,
+			enable = true,
 			options = {
 				-- If lualine is installed tabline will use separators configured in lualine by default.
 				-- These options can be used to override those settings.
@@ -13,10 +13,10 @@ return {
 				-- show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
 				show_devicons = true, -- this shows devicons in buffer section
 				-- show_bufnr = false, -- this appends [bufnr] to buffer section,
-				-- show_filename_only = false, -- shows base filename only instead of relative path in filename
+				show_filename_only = true, -- shows base filename only instead of relative path in filename
 				-- modified_icon = "+ ", -- change the default modified icon
 				-- modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
-				-- show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+				show_tabs_only = true, -- this shows only tabs instead of tabs + buffers
                 show_last_separator = true, -- Show separator after the last buffer or tab (default = false)
 			},
 		})
