27191c3b07c5accd8fa17db2c8697c40980dac2e
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2024-06-02 18:27:41 +0200

    2/6/24 add gitsigns
    After install of gitsigns

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,13 +1,10 @@
-
--- tokyonight themes      
-return { 
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    name = "tokyonight",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "tokyonight-storm" -- storm, night, day, moon
-    end
+-- tokyonight themes
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	name = "tokyonight",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("tokyonight-storm") -- storm, night, day, moon
+	end,
 }
-
