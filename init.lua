a7c8923bb9ea800d4fbac7752a45974fdb9fa01f
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2024-06-02 20:22:00 +0200

    2/6/24 Neotree starts automatically

--- init.lua
+++ init.lua
@@ -12,8 +12,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Launch neotree automatically
+
 require("vim-options")
 
 -- Loads programs when starting
 require("lazy").setup("plugins")
-
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,24 @@
-
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    end
-}
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 
+		-- Launch neotree automatically
+		vim.api.nvim_create_augroup("neotree", {})
+		vim.api.nvim_create_autocmd("UiEnter", {
+			desc = "Open Neotree automatically",
+			group = "neotree",
+			callback = function()
+				if vim.fn.argc() == 0 then
+					vim.cmd("Neotree toggle")
+				end
+			end,
+		})
+	end,
+}
